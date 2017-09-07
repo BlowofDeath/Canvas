8e227176250c3f1f67038a7c047d25004fb55ab8
BlowofDeath <jabolovp@gmail.com>
2017-09-07 16:48:00 +0200

    Stawianie serwera

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+  app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server);
+ 
+server.listen(3000);
+ 
+app.get('/',function(req,res){
+  app.use(express.static(__dirname + '/public'));
+  
+});
+ 
+ 
+io.sockets.on('connection', function (socket) {
+ 
+   console.log("Socket connected.");
+ 
+   socket.on('message', function(msg){
+     io.emit('message', msg);
+   });
+  
+});
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Wymagana instalacj npm:
+
+
+npm install --save
+npm install express --save
+npm install --save socket.io
+
+//w razie b³edów usun¹æ dependencies 
