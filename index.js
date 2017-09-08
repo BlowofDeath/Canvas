a5a5935a5d7f53ee500bf33fb9d5325f25d64348
BlowofDeath <jabolovp@gmail.com>
2017-09-08 03:36:09 +0200

    Wprowadzenie zdalnego sterowania paletkami Do zrobienia: 1. zsynchronizowanie piłki 2. menu i dobieranie graczy

--- index.js
+++ index.js
@@ -9,14 +9,54 @@ app.use(express.static('public'));
 server.listen(3000);
 console.log('server started');
 
-var PLAYERS = {};
+//var PLAYERS = {};
+
+var p1;
+var p2;
 
 io.sockets.on('connection', function(socket){
-	socket.id = Math.random();
+	//PLAYERS[socket.id] = socket;
 
-	socket.on('rocket', function(y){
-		socket.emit('rocket_res', y);
-		console.log('connected' + y);
+	console.log(socket.id + ': has connected');
+
+	socket.on("playertwo", function(socketid){
+
+		p2 = socket;
+		console.log("Player two is: " + p2.id);
+		position();
+
 	});
+	socket.on("playerone", function(socketid){
+
+		p1 = socket;
+		console.log("Player one is: " + p1.id);
+		position();
+
+	});
+
+
 });
+
+function position(){
+	if(p1 != undefined && p2 != undefined)
+	{
+		p1.on('rocket', function(y){
+
+				//var socket = PLAYERS[i];
+			p2.emit('rocket_res', y);
+
+		});
+		p2.on('rocket_2', function(y){
+
+				//var socket = PLAYERS[i];
+			p1.emit('rocket_2_res', y);
+
+		});
+	}
+}
+
+
+
+
+
 
